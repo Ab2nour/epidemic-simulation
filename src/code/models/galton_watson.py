518b1892e923cbeb9ea513bd94f8d98a605bf3af
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2024-01-14 18:05:47 +0100

    feat(plot): add history and Zn/n

--- src/code/models/galton_watson.py
+++ src/code/models/galton_watson.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy.stats import rv_discrete
 
 
@@ -18,7 +19,11 @@ def galton_watson(loi: rv_discrete, nb_epoques: int):
 class GaltonWatson:
     def __init__(self, loi: rv_discrete, nb_descendants: int = 1):
         self.loi = loi
+        self.m = loi.mean()
+
         self.nb_descendants = nb_descendants
+        self.historique_nb_descendants: list[int] = [nb_descendants]
+
         self.liste_descendants: list[list[int]] = []  # fixme: ce sont des listes de np.ndarray
         self.n: int = 0  # numéro de l'époque
 
@@ -39,10 +44,52 @@ class GaltonWatson:
         while epoque_actuelle < nb_epoques and self.nb_descendants > 0:
             liste_descendants = self.loi.rvs(size=self.nb_descendants)
             self.liste_descendants.append(liste_descendants)
+
             self.nb_descendants = np.sum(liste_descendants)
+            self.historique_nb_descendants.append(self.nb_descendants)
 
             epoque_actuelle += 1
 
         self.n = len(self.liste_descendants)
 
         return self.nb_descendants
+
+    def plot_historique_descendants(self, log: bool = False, affiche_moyenne: bool = False) -> None:
+        """
+        Affiche l'historique des descendants.
+
+        Parameters
+        ----------
+        log: active l'échelle logarithmique
+        affiche_moyenne: affiche l'évolution de E[L]^m
+
+        Returns
+        -------
+
+        """
+        plt.plot(self.historique_nb_descendants, label="Nombre de descendants")
+
+        if affiche_moyenne:
+            x = np.arange(self.n)
+            plt.plot(x, self.m ** x, label=r"Nombre de descendants prévu ($\mathbb{E}[L]^m$)")
+
+        plt.title("Historique du nombre de descendants")
+        plt.xlabel("Numéro d'époque n")
+        plt.ylabel("Nombre de descendants")
+
+        plt.legend()
+
+        if log:
+            plt.yscale("log")
+
+    def plot_zn_sur_n(self, log: bool = False) -> None:
+        hist = np.array(self.historique_nb_descendants[1:])
+        r = range(1, len(hist) + 1)  # fixme: choisir un meilleur nom de variable
+        plt.plot(r, hist / r)
+
+        plt.title(r"Évolution de $\dfrac{Z_n}{n}$")
+        plt.xlabel("Numéro d'époque n")
+        plt.ylabel(r"$\dfrac{Z_n}{n}$")
+
+        if log:
+            plt.yscale("log")
